d2775e54eb32eb5d51659972be06791a70f7b511
arunpogula <arun.pogula@mycompany.com>
2021-11-11 17:48:50 +0530

    aruncommit

--- myproject/webapp/models.py
+++ myproject/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    sname = models.CharField(max_length=100)
+    sid = models.IntegerField()
+    sloc = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.sname
+
+
+        
