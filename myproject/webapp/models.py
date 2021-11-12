573302753c56417eaf445e9aeff128e0c72359b4
arunpogula <arun.pogula@mycompany.com>
2021-11-12 08:17:23 +0530

    new_model_changes

--- myproject/webapp/models.py
+++ myproject/webapp/models.py
@@ -3,14 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class School(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Student(models.Model):
     sname = models.CharField(max_length=100)
-    sid = models.IntegerField()
+    sid = models.IntegerField(unique=True)
     sloc = models.CharField(max_length=100)
-
+    school = models.ForeignKey(School, on_delete= models.CASCADE)
 
     def __str__(self):
         return self.sname
+
+
+
+
 
 
         
